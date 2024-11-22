31e8fa23ad1a36e94ff2ad91502db82e11efe4f9
Jorge Morales <140109596+mordacay@users.noreply.github.com>
2024-11-21 22:36:34 -0400

    Revert "Pagina debugguers"

--- Debugguers/script.js
+++
@@ -1,9 +0,0 @@
-const menu = document.getElementById('menu');
-const sidebar = document.getElementById('sidebar');
-const main = document.getElementById('main');
-
-
-menu.addEventListener('click',()=>{
-    sidebar.classList.toggle('menu-toggle');
-    main.classList.toggle('menu-toggle');
-});
